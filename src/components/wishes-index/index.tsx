43255401da895f6e0100264fccba88b04c9b23af
Evg Sh <>
2023-07-09 09:59:18 +0300

    content outlet inset: 0

--- src/components/wishes-index/index.tsx
+++ src/components/wishes-index/index.tsx
@@ -1,11 +1,37 @@
 import { Link, useLoaderData } from "react-router-dom";
-import type { WishPaginationDto } from "../../api/model";
+import type { WishDto, WishPaginationDto } from "../../api/model";
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 export const WishesIndex = () => {
   const wishes = useLoaderData() as {
     body: WishPaginationDto;
     error: any;
   };
+
+  const columnHelper = createColumnHelper<WishDto>();
+
+  const columns = [
+    columnHelper.accessor("patient", {
+      header: () => <span>patient</span>,
+      cell: (props) => {
+        // eslint-disable-next-line react/prop-types
+        console.log(props, props.getValue());
+        return "cell";
+      },
+    }),
+  ];
+
+  const table = useReactTable({
+    data: wishes.body?.elements || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div>
@@ -15,6 +41,36 @@ export const WishesIndex = () => {
           <Link to={`${e.id}`}>{e.title}</Link>
         </div>
       ))}
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <pre>{JSON.stringify(wishes, null, 2)}</pre>
     </div>
   );
 };
