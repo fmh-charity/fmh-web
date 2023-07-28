44ffe8f397526bd1eb12543b2553135658d9836b
Evg Sh <>
2023-07-28 15:56:06 +0300

    status date

--- src/components/cells/executeDate/index.tsx
+++ src/components/cells/executeDate/index.tsx
@@ -5,32 +5,33 @@ import dayjs from "dayjs";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import styles from "./index.module.less";
 import clsx from "clsx";
+import { numberBetween } from "../../../common/utils";
 
 dayjs.extend(isSameOrBefore);
 
 export const ExecuteDate: React.FC<{ row: Row<WishDto> }> = ({ row }) => {
-  const hourDiff = Math.abs(
-    dayjs.utc(row.original.planExecuteDate).diff(dayjs().utc(true), "hour")
-  );
-
-  const statusRed = hourDiff < 6;
-  const statusOrange = hourDiff < 12;
+  const hourDiff = dayjs
+    .utc(row.original.planExecuteDate)
+    .diff(dayjs().utc(true), "hour");
 
   const dateDesktop = row.original.planExecuteDate
     ? dayjs.utc(row.original.planExecuteDate).format("YYYY-MM-DD")
     : "";
 
-  const title = "До завершения осталось " + hourDiff + " часов ";
+  const title =
+    hourDiff >= 0
+      ? "До завершения осталось " + hourDiff + " часов "
+      : "Просрочено";
 
   return (
     <div title={title} className={styles.wrapper}>
       {dateDesktop}
-      {(statusRed || statusOrange) && dateDesktop && (
+      {dateDesktop && (
         <div
           className={clsx({
             [styles.circle]: true,
-            [styles.red]: statusRed,
-            [styles.orange]: statusOrange && !statusRed,
+            [styles.red]: numberBetween(hourDiff, 0, 6) || hourDiff <= 0,
+            [styles.orange]: numberBetween(hourDiff, 7, 12),
           })}
         />
       )}
