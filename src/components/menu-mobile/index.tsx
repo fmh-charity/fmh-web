c458e794be75db0b3f5729c50973148c398cd9e2
Evg Sh <>
2023-08-01 00:16:37 +0300

    prevent close mobile menu

--- src/components/menu-mobile/index.tsx
+++ src/components/menu-mobile/index.tsx
@@ -5,7 +5,16 @@ export const MenuMobile = ({ close }: { close: () => void }) => {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div className={styles.menu} onClick={close}>
-      <Sidebar />
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      <div
+        className={styles.wrapper}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <Sidebar />
+      </div>
     </div>
   );
 };
