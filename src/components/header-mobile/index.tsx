313edc22c05f7c10e08dce8b077df92f86c9cced
Evg Sh <>
2023-08-01 09:07:01 +0300

    adapt mobile menu

--- src/components/header-mobile/index.tsx
+++ src/components/header-mobile/index.tsx
@@ -1,23 +1,29 @@
-import { useRoutes } from "react-router-dom";
+import { Link, useNavigate, useRoutes } from "react-router-dom";
 import { Icon } from "../icon";
 import styles from "./index.module.less";
 import { headerRoutes } from "../header";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { MenuMobile } from "../menu-mobile";
 
 export const HeaderMobile = () => {
   const [open, setOpen] = useState(false);
   const routes = useRoutes(headerRoutes);
+  const navigate = useNavigate();
+  const close = useCallback(() => {
+    setOpen(false);
+  }, []);
   return (
     <div className={styles.header}>
       <button onClick={() => setOpen((c) => !c)}>
         <Icon.Menu24 />
       </button>
-      <div className={styles.routes}>{routes}</div>
-      <button>
+      <div className={styles.routes}>
+        <Link to="/profile">{routes}</Link>
+      </div>
+      <button onClick={() => navigate("/logout")}>
         <Icon.Notificatons24 />
       </button>
-      {open && <MenuMobile close={() => setOpen(false)} />}
+      {open && <MenuMobile close={close} />}
     </div>
   );
 };
--- src/components/menu-mobile/index.tsx
+++ src/components/menu-mobile/index.tsx
@@ -1,20 +1,35 @@
-import { Sidebar } from "../menu";
+import { useRouteLoaderData } from "react-router-dom";
+import { Menu } from "../menu";
+import { footerMenu, menu } from "../menu/menuItems";
 import styles from "./index.module.less";
+import type { UserInfoDto } from "../../api/model";
+import { getRoleByRank } from "../../common/roles";
 
 export const MenuMobile = ({ close }: { close: () => void }) => {
+  const data = useRouteLoaderData("app") as UserInfoDto;
+
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div className={styles.menu} onClick={close}>
+    <>
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-      <div
-        className={styles.wrapper}
-        onClick={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-      >
-        <Sidebar />
+      <div className={styles.overlay} onClick={close}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <img src="/images/avatar_mock.png" alt="avatar" />
+          <div>
+            <div className={styles.name}>
+              {data?.firstName} {data?.lastName}
+            </div>
+            <div className={styles.roles}>
+              {data?.roles ? (
+                <div>{getRoleByRank(data?.roles)?.roleName}</div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <div className={styles.separator} />
+        <Menu items={menu} close={close} />
+        <Menu items={footerMenu} close={close} />
       </div>
-    </div>
+    </>
   );
 };
