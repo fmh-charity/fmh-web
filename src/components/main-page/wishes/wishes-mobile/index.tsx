d68643a296f9c8b2e116a5233833553cf851cc7e
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2024-06-08 13:45:40 +0500

    feat: add "show all wishes" button to mobile version of main page, fix some styles

--- src/components/main-page/wishes/wishes-mobile/index.tsx
+++ src/components/main-page/wishes/wishes-mobile/index.tsx
@@ -2,15 +2,18 @@ import type { Table } from "@tanstack/react-table";
 import styles from "./index.module.less";
 import { CardsWishes } from "../../../table-section-mobile/cards/wishes";
 import { TableSectionMobile } from "./table-section/table-section";
+import { ButtonLink } from "../../../button-link";
 
 export const MainWishesIndexMobile = ({ table }: { table: Table<any> }) => {
   return (
     <div className={styles.wishes}>
       <p className={styles.h2}>Просьбы</p>
-      <TableSectionMobile
-        table={table}
-        Card={CardsWishes}
-      />
+      <TableSectionMobile table={table} Card={CardsWishes} />
+      <div className={styles.buttons}>
+        <ButtonLink intent="primary" justify="center" to="../wishes">
+          Просмотреть все просьбы
+        </ButtonLink>
+      </div>
     </div>
   );
 };
