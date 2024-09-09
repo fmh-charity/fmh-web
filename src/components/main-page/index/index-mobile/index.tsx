d2592a70875dd39b888409c0ac980af4852bb74b
Dmitry Chigrin <98915479+ChigrinDmitry@users.noreply.github.com>
2024-09-09 20:35:41 +0500

    fix: margins on mobile version of main page (#124)

--- src/components/main-page/index/index-mobile/index.tsx
+++ src/components/main-page/index/index-mobile/index.tsx
@@ -1,12 +1,13 @@
 import MainPageNews from "../../news";
 import { MainWishesIndexMobile } from "../../wishes/wishes-mobile";
 import { Table } from "@tanstack/react-table";
+import styles from "./index.module.less";
 
 const MainPageMobile = ({ table }: { table: Table<any> }) => {
   return (
-    <div>
+    <div className={styles.wishes}>
       <MainPageNews />
-      <MainWishesIndexMobile table={table} />
+      <MainWishesIndexMobile table={table}/>
     </div>
   );
 };
